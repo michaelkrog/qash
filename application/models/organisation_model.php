df10b0f3cf421039ee94ba0600c6fdcf93776285
ShoppinNet.com <ShoppinNet.com@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2011-08-14 20:33:55 +0000

    Bug fix

--- application/models/organisation_model.php
+++ application/models/organisation_model.php
@@ -13,10 +13,7 @@ class Organisation_model extends CI_Model {
 	$this->load->model('service/organisation_service');
 
 	$website = $this->website_service->get();
-
-	// !!! Can't get organisation integer value from website entity
-	die($website->get_organisation());
-	$data = $this->organisation_service->read($website->get_organisation());
+	$data = $website->get_organisation();
 
 	$this->load->library('form');
 
