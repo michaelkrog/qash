edbaa1a4b1f3bf5f1b4e151e68afd5cdecd4951e
ShoppinNet.com <ShoppinNet.com@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2011-08-19 20:43:11 +0000

    Setup spring controllers. Could not build and test, due to the following error:
    Failed to execute goal on project shopsystem: Could not resolve dependencies for project dk.apaq:shopsystem:war:2.0-SNAPSHOT: The following artifacts could not be resolved: javax.sql:jdbc-stdext:jar:2.0, javax.transaction:jta:jar:1.0.1B: Could not find artifact javax.sql:jdbc-stdext:jar:2.0 in vaadin-addons (http://maven.vaadin.com/vaadin-addons)

---
+++ src/main/java/dk/apaq/shopsystem/controller/HomeController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.apaq.shopsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Martin Christensen
+ */
+
+@Controller
+public class HomeController  {
+
+    @RequestMapping("/home")
+    public ModelAndView helloWorld() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("home");
+        mav.addObject("message", "Shopsystem V.2");
+        return mav;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : home
+    Created on : Aug 18, 2011, 7:08:19 PM
+    Author     : Martin Christensen
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>${message}</h1>
+    </body>
+</html>
