a3100851ed4a852d47819020cfad7e4ca17a7177
michael.krog <michael.krog@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2011-08-20 06:37:09 +0000

    Fixed some dependencies in pom(Need cleanup later) Fixed missing namespace in springcontext.xml.

--- src/main/java/dk/apaq/shopsystem/controller/HomeController.java
+++ src/main/java/dk/apaq/shopsystem/controller/HomeController.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package dk.apaq.shopsystem.controller;
 
 import org.springframework.stereotype.Controller;
