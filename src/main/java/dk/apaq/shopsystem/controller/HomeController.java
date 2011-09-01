dc0c2fdc9f369ecc443255e760999b7f4a79a437
ShoppinNet.com <ShoppinNet.com@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2011-09-01 16:07:25 +0000

    Added vaadin add-on ReindeerMods. Needs compiling.

--- src/main/java/dk/apaq/shopsystem/controller/HomeController.java
+++ src/main/java/dk/apaq/shopsystem/controller/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController  {
 
-    @RequestMapping("/home")
+    @RequestMapping("/")
     public ModelAndView helloWorld() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("layout");
