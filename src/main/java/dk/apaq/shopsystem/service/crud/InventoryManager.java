3a1f322d3446c5f8acd230e0b7315676f0b0ef91
michael.krog <michael.krog@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2011-09-12 10:23:43 +0000

    Added Javadoc to InventoryManager.

--- src/main/java/dk/apaq/shopsystem/service/crud/InventoryManager.java
+++ src/main/java/dk/apaq/shopsystem/service/crud/InventoryManager.java
@@ -1,16 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package dk.apaq.shopsystem.service.crud;
 
 /**
- *
+ * A simple interface for handling inventory.
  * @author michaelzachariassenkrog
  */
 public interface InventoryManager {
 
+    /**
+     * Pulls an item from Stock.
+     * @param itemId The itemid of the item.
+     * @param quantity The quantity to pull.
+     */
     void pullFromStock(String itemId, double quantity);
+
+    /**
+     * Checks wether the stock/inventory manages an item with the given itemid.
+     */
     boolean isStockItem(String itemId);
 }
