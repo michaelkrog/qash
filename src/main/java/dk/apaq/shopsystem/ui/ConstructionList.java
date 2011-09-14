2a6b793e3c43f5eb3485dc09c0cb73df0f564fe4
ShoppinNet.com <ShoppinNet.com@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2011-09-14 19:22:48 +0000

    Test commit

---
+++ src/main/java/dk/apaq/shopsystem/ui/ConstructionList.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.apaq.shopsystem.ui;
+
+/**
+ *
+ * @author Martin Christensen
+ */
+public class ConstructionList {
+    
+}
---
+++ src/main/java/dk/apaq/shopsystem/ui/common/Grid.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.apaq.shopsystem.ui.common;
+
+/**
+ *
+ * @author Martin Christensen
+ */
+public class Grid {
+    
+}
