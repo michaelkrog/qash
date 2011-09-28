ace4fced3f82fc762a1fc0bdf910afd4fa9cc123
michael.krog <michael.krog@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2011-09-28 06:38:50 +0000

    More work on Domain, Page, Website and Components.

--- shopsystem-webapp/src/main/java/dk/apaq/shopsystem/filter/DataContextFilter.java
+++ shopsystem-webapp/src/main/java/dk/apaq/shopsystem/filter/DataContextFilter.java
@@ -1,7 +1,12 @@
 package dk.apaq.shopsystem.filter;
 
+import dk.apaq.filter.core.CompareFilter;
+import dk.apaq.filter.core.ContainsFilter;
 import dk.apaq.shopsystem.context.DataContext;
+import dk.apaq.shopsystem.entity.Domain;
 import dk.apaq.shopsystem.entity.Organisation;
+import dk.apaq.shopsystem.entity.Website;
+import dk.apaq.shopsystem.service.ContainsDomainFilter;
 import dk.apaq.shopsystem.service.SystemService;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -14,6 +19,8 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -22,53 +29,62 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
  * @author krog
  */
 public class DataContextFilter implements Filter {
-
+    
+    private static final Logger LOG = LoggerFactory.getLogger(DataContextFilter.class);
     private FilterConfig filterConfig = null;
     private ApplicationContext ac;
     private SystemService service;
-
+    
     public DataContextFilter() {
-    } 
+    }
 
     /**
      *
      */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-	throws IOException, ServletException {
-
+            throws IOException, ServletException {
+        
         String domain = request.getServerName();
-        //resolve organsiation from domain/servername
 
-        //For now its just the first we find
-        List<String> ids = service.getOrganisationCrud().listIds();
-        if(!ids.isEmpty()) {
-            Organisation org = service.getOrganisationCrud().read(ids.get(0));
-            DataContext.setService(service.getOrganisationService(org));
-        }
-	
-	try {
-            if(DataContext.getService() == null) {
-                throw new ServletException("No sites available at the host '" + domain + "'.");
+        //Optimize this by putting results in a map and listen for updateevents.
+        dk.apaq.filter.Filter filter = new ContainsDomainFilter("domains", domain);
+        List<String> idlist = service.getWebsites().listIds(filter, null);
+        
+        if (!idlist.isEmpty()) {
+            //resolve organsiation from domain/servername
+            Website site = service.getWebsites().read(idlist.get(0));
+            Organisation org = site.getOrganisation();
+            if (org != null) {
+                DataContext.setService(service.getOrganisationService(org));
             }
-	    chain.doFilter(request, response);
-	}
-	catch(Throwable t) {
-            if (t instanceof ServletException) throw (ServletException)t;
-	    if (t instanceof IOException) throw (IOException)t;
-	    sendProcessingError(t, response);
-	} finally {
+        }
+        
+        try {
+            if (DataContext.getService() == null) {
+                LOG.debug("No service available for domain '" + domain + "'");
+            }
+            chain.doFilter(request, response);
+        } catch (Throwable t) {
+            if (t instanceof ServletException) {
+                throw (ServletException) t;
+            }
+            if (t instanceof IOException) {
+                throw (IOException) t;
+            }
+            sendProcessingError(t, response);
+        } finally {
             //Reset the datacontext
             DataContext.detach();
         }
-
+        
     }
-    
+
     /**
      * Return the filter configuration object for this filter.
      */
     public FilterConfig getFilterConfig() {
-	return (this.filterConfig);
+        return (this.filterConfig);
     }
 
     /**
@@ -77,75 +93,72 @@ public class DataContextFilter implements Filter {
      * @param filterConfig The filter configuration object
      */
     public void setFilterConfig(FilterConfig filterConfig) {
-	this.filterConfig = filterConfig;
+        this.filterConfig = filterConfig;
     }
 
     /**
      * Destroy method for this filter 
      */
-    public void destroy() { 
+    public void destroy() {        
     }
 
     /**
      * Init method for this filter 
      */
     @Override
-    public void init(FilterConfig filterConfig) { 
-	this.filterConfig = filterConfig;
-	if (filterConfig != null) {
-	    ac = WebApplicationContextUtils.getWebApplicationContext(filterConfig.getServletContext());
+    public void init(FilterConfig filterConfig) {        
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            ac = WebApplicationContextUtils.getWebApplicationContext(filterConfig.getServletContext());
             service = ac.getBean(SystemService.class);
-	}
+        }
     }
-
-
+    
     private void sendProcessingError(Throwable t, ServletResponse response) {
-	String stackTrace = getStackTrace(t); 
+        String stackTrace = getStackTrace(t);        
+        
+        if (stackTrace != null && !stackTrace.equals("")) {
+            try {
+                response.setContentType("text/html");
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                PrintWriter pw = new PrintWriter(ps);                
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
 
-	if(stackTrace != null && !stackTrace.equals("")) {
-	    try {
-		response.setContentType("text/html");
-		PrintStream ps = new PrintStream(response.getOutputStream());
-		PrintWriter pw = new PrintWriter(ps); 
-		pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
-		    
-		// PENDING! Localize this for next official release
-		pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n"); 
-		pw.print(stackTrace); 
-		pw.print("</pre></body>\n</html>"); //NOI18N
-		pw.close();
-		ps.close();
-		response.getOutputStream().close();
-	    }
-	    catch(Exception ex) {}
-	}
-	else {
-	    try {
-		PrintStream ps = new PrintStream(response.getOutputStream());
-		t.printStackTrace(ps);
-		ps.close();
-		response.getOutputStream().close();
-	    }
-	    catch(Exception ex) {}
-	}
+                // PENDING! Localize this for next official release
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");                
+                pw.print(stackTrace);                
+                pw.print("</pre></body>\n</html>"); //NOI18N
+                pw.close();
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        } else {
+            try {
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                t.printStackTrace(ps);
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        }
     }
-
+    
     public static String getStackTrace(Throwable t) {
-	String stackTrace = null;
-	try {
-	    StringWriter sw = new StringWriter();
-	    PrintWriter pw = new PrintWriter(sw);
-	    t.printStackTrace(pw);
-	    pw.close();
-	    sw.close();
-	    stackTrace = sw.getBuffer().toString();
-	}
-	catch(Exception ex) {}
-	return stackTrace;
+        String stackTrace = null;
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+            pw.close();
+            sw.close();
+            stackTrace = sw.getBuffer().toString();
+        } catch (Exception ex) {
+        }
+        return stackTrace;
     }
-
+    
     public void log(String msg) {
-	filterConfig.getServletContext().log(msg); 
+        filterConfig.getServletContext().log(msg);        
     }
-
 }
