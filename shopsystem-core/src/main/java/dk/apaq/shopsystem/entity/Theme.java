8f6150983df1645a51e55f1eb7e0affcca982b59
michael.krog <michael.krog@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2011-10-31 16:02:29 +0000

    Extract logic for parsing template information into TemaplteInformationExtractor.

--- shopsystem-core/src/main/java/dk/apaq/shopsystem/entity/Theme.java
+++ shopsystem-core/src/main/java/dk/apaq/shopsystem/entity/Theme.java
@@ -1,23 +1,13 @@
 package dk.apaq.shopsystem.entity;
 
-import dk.apaq.shopsystem.rendering.CmsTag;
-import dk.apaq.shopsystem.rendering.TemplateMarkupParserFactory;
-import dk.apaq.shopsystem.rendering.VfsResourceStream;
+import dk.apaq.shopsystem.rendering.template.TemplateInformationExtractor;
 import dk.apaq.vfs.Directory;
 import dk.apaq.vfs.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import org.apache.wicket.markup.ComponentTag;
-import org.apache.wicket.markup.Markup;
-import org.apache.wicket.markup.MarkupElement;
-import org.apache.wicket.markup.MarkupParser;
-import org.apache.wicket.markup.MarkupResourceStream;
-import org.apache.wicket.markup.WicketTag;
-import org.apache.wicket.util.resource.ResourceStreamNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,16 +16,11 @@ import org.slf4j.LoggerFactory;
  */
 public class Theme extends AbstractModule implements Serializable {
 
-    private static final TemplateMarkupParserFactory MARKUP_PARSER_FACTORY = new TemplateMarkupParserFactory();
+    private static final TemplateInformationExtractor TEMPLATE_INFORMATION_EXTRACTOR = new TemplateInformationExtractor();
     private static final Logger LOG = LoggerFactory.getLogger(Theme.class);
     
     private List<Template> templateList;
     private SuffixNodeFilter nodeFilter = new SuffixNodeFilter("html");
-    
-    private class TemplateInfo {
-        private List<Placeholder> placeholders = new ArrayList<Placeholder>();
-        private List<ComponentInformation> componentInformations = new ArrayList<ComponentInformation>();
-    }
     
     public Theme(Directory dir) throws IOException {
         super(dir);
@@ -59,13 +44,13 @@ public class Theme extends AbstractModule implements Serializable {
                 String codeFileName = name + ".code";
                 File codeFile = null;
                 try {
-                    TemplateInfo templateInfo = parseTemplateInfo(markupFile);
+                    TemplateInformationExtractor.TemplateInformation info = TEMPLATE_INFORMATION_EXTRACTOR.readTemplateInformation(markupFile);
                     
                     if(dir.hasFile(codeFileName)) {
                         codeFile = dir.getFile(codeFileName);
                     }
                     
-                    newList.add(new Template(markupFile.getBaseName(), templateInfo.placeholders, templateInfo.componentInformations, markupFile, codeFile));
+                    newList.add(new Template(markupFile.getBaseName(), info.getPlaceholders(), info.getComponentInformations(), markupFile, codeFile));
                     
                 } catch (IOException ex) {
                     //Cannot find file - warn about it but otherwise ignore it.
@@ -78,84 +63,6 @@ public class Theme extends AbstractModule implements Serializable {
     }
 
     
-    
-    
-    private TemplateInfo parseTemplateInfo(File file) throws IOException {
-        MarkupParser parser = MARKUP_PARSER_FACTORY.newMarkupParser(new MarkupResourceStream(new VfsResourceStream(file)));
-        TemplateInfo templateInfo = new TemplateInfo();
-        
-        try {
-            Markup m = parser.parse();
-            boolean inContainer = false;
-            boolean inCmsComponentTag = false;
-            ComponentInformation componentInformation = null;
-            Placeholder currentPlaceHolder = null;
-            
-            Iterator<MarkupElement> it =  m.iterator();
-            while(it.hasNext()) {
-                MarkupElement me = it.next();
-                if(me instanceof WicketTag) {
-                    WicketTag ct = (WicketTag)me;
-                    
-                    if(ct.isClose() && ct.isContainerTag() && inContainer) {
-                        inContainer = false;
-                        continue;
-                    }
-                    
-                    if(!ct.isClose() && !ct.hasBehaviors() && !ct.isAutoComponentTag() && !inContainer) {
-                        if(ct.isContainerTag()) {
-                            currentPlaceHolder = new Placeholder(ct.getId(), true);
-                            inContainer = true;
-                        } else {
-                            currentPlaceHolder = new Placeholder(ct.getId(), false);
-                        }
-                        
-                        templateInfo.placeholders.add(currentPlaceHolder);
-                        continue;
-                    }
-                }
-                
-                if(me instanceof CmsTag) {
-                    CmsTag ct = (CmsTag) me;
-                    
-                    if(!ct.isClose() && ct.isParameterTag() && inCmsComponentTag) {
-                        String name = ct.getAttribute("name");
-                        String value = ct.getAttribute("value");
-                        ComponentParameter componentParameter = new ComponentParameter();
-                        componentParameter.setString(value);
-                        componentInformation.getParameterMap().put(name, componentParameter);
-                        continue;
-                    }
-                    
-                    if(ct.isClose() && ct.isComponentTag() && inCmsComponentTag) {
-                        inCmsComponentTag = false;
-                        templateInfo.componentInformations.add(componentInformation);
-                        componentInformation = null;
-                        
-                        continue;
-                    }
-                    
-                    if(!ct.isClose() && ct.isComponentTag() && !inCmsComponentTag && inContainer) {
-                        inCmsComponentTag = true;
-                        String module = ct.getAttribute("module");
-                        String component = ct.getAttribute("component");
-                        componentInformation = new ComponentInformation();
-                        componentInformation.setModuleName(module);
-                        componentInformation.setComponentName(component);
-                        componentInformation.setPlaceholderName(currentPlaceHolder.getId());
-                        continue;
-                    }
-                }
-                
-                
-            }
-        } catch (ResourceStreamNotFoundException ex) {
-            throw new IOException(ex);
-        }
-        
-        return templateInfo;
-    }
-
     @Override
     protected String getBundleSuffix() {
         return "theme";
---
+++ shopsystem-core/src/main/java/dk/apaq/shopsystem/rendering/template/TemplateInformationExtractor.java
@@ -0,0 +1,157 @@
+package dk.apaq.shopsystem.rendering.template;
+
+import dk.apaq.shopsystem.entity.ComponentInformation;
+import dk.apaq.shopsystem.entity.ComponentParameter;
+import dk.apaq.shopsystem.entity.Placeholder;
+import dk.apaq.shopsystem.rendering.CmsTag;
+import dk.apaq.shopsystem.rendering.CmsTagIdentifier;
+import dk.apaq.shopsystem.rendering.VfsResourceStream;
+import dk.apaq.vfs.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.wicket.markup.Markup;
+import org.apache.wicket.markup.MarkupElement;
+import org.apache.wicket.markup.MarkupParser;
+import org.apache.wicket.markup.MarkupResourceStream;
+import org.apache.wicket.markup.WicketTag;
+import org.apache.wicket.markup.parser.IMarkupFilter;
+import org.apache.wicket.markup.parser.filter.WicketLinkTagHandler;
+import org.apache.wicket.markup.parser.filter.WicketRemoveTagHandler;
+import org.apache.wicket.util.resource.ResourceStreamNotFoundException;
+
+/**
+ *
+ * @author krog
+ */
+public class TemplateInformationExtractor {
+    
+    private static final Class[] IGNORED_CLASSES = {WicketRemoveTagHandler.class,WicketLinkTagHandler.class};
+
+    private class TemplateMarkupParser extends MarkupParser {
+
+        public TemplateMarkupParser(MarkupResourceStream resource) {
+            super(resource);
+        }
+
+        @Override
+        protected MarkupFilterList initializeMarkupFilters(Markup markup) {
+            MarkupFilterList list = new MarkupFilterList();
+            list.add(new CmsTagIdentifier(markup.getMarkupResourceStream()));
+            MarkupFilterList superList = super.initializeMarkupFilters(markup);
+            
+            Iterator<IMarkupFilter> it = superList.iterator();
+            while(it.hasNext()) {
+                IMarkupFilter filter = it.next();
+                for(Class ignoredClass : IGNORED_CLASSES) {
+                    if(filter.getClass() == ignoredClass) {
+                        it.remove();
+                        break;
+                    }
+                }
+            }
+            
+            list.addAll(superList);
+            return list;
+        }
+        
+        
+    }
+    
+    public class TemplateInformation {
+        private final List<ComponentInformation> componentInformations;
+        private final List<Placeholder> placeholders;
+
+        private TemplateInformation(List<ComponentInformation> componentInformations, List<Placeholder> placeholders) {
+            this.componentInformations = componentInformations;
+            this.placeholders = placeholders;
+        }
+
+        public List<ComponentInformation> getComponentInformations() {
+            return componentInformations;
+        }
+
+        public List<Placeholder> getPlaceholders() {
+            return placeholders;
+        }
+    }
+    
+    public TemplateInformation readTemplateInformation(File file) throws IOException {
+        MarkupParser parser = new TemplateMarkupParser(new MarkupResourceStream(new VfsResourceStream(file)));
+        List<ComponentInformation> componentInformations = new ArrayList<ComponentInformation>();
+        List<Placeholder> placeholders = new ArrayList<Placeholder>();
+        TemplateInformation templateInfo = new TemplateInformation(componentInformations, placeholders);
+        
+        try {
+            Markup m = parser.parse();
+            boolean inContainer = false;
+            boolean inCmsComponentTag = false;
+            ComponentInformation componentInformation = null;
+            Placeholder currentPlaceHolder = null;
+            
+            Iterator<MarkupElement> it =  m.iterator();
+            while(it.hasNext()) {
+                MarkupElement me = it.next();
+                if(me instanceof WicketTag) {
+                    WicketTag ct = (WicketTag)me;
+                    
+                    if(ct.isClose() && ct.isContainerTag() && inContainer) {
+                        inContainer = false;
+                        continue;
+                    }
+                    
+                    if(!ct.isClose() && !ct.hasBehaviors() && !ct.isAutoComponentTag() && !inContainer) {
+                        if(ct.isContainerTag()) {
+                            currentPlaceHolder = new Placeholder(ct.getId(), true);
+                            inContainer = true;
+                        } else {
+                            currentPlaceHolder = new Placeholder(ct.getId(), false);
+                        }
+                        
+                        placeholders.add(currentPlaceHolder);
+                        continue;
+                    }
+                }
+                
+                if(me instanceof CmsTag) {
+                    CmsTag ct = (CmsTag) me;
+                    
+                    if(!ct.isClose() && ct.isParameterTag() && inCmsComponentTag) {
+                        String name = ct.getAttribute("name");
+                        String value = ct.getAttribute("value");
+                        ComponentParameter componentParameter = new ComponentParameter();
+                        componentParameter.setString(value);
+                        componentInformation.getParameterMap().put(name, componentParameter);
+                        continue;
+                    }
+                    
+                    if(ct.isClose() && ct.isComponentTag() && inCmsComponentTag) {
+                        inCmsComponentTag = false;
+                        componentInformations.add(componentInformation);
+                        componentInformation = null;
+                        
+                        continue;
+                    }
+                    
+                    if(!ct.isClose() && ct.isComponentTag() && !inCmsComponentTag && inContainer) {
+                        inCmsComponentTag = true;
+                        String module = ct.getAttribute("module");
+                        String component = ct.getAttribute("component");
+                        componentInformation = new ComponentInformation();
+                        componentInformation.setModuleName(module);
+                        componentInformation.setComponentName(component);
+                        componentInformation.setPlaceholderName(currentPlaceHolder.getId());
+                        continue;
+                    }
+                }
+                
+                
+            }
+        } catch (ResourceStreamNotFoundException ex) {
+            throw new IOException(ex);
+        }
+        
+        return templateInfo;
+    }
+}
