4d4a1b2b27f30c717a5a4e3ef19bde23e95d075f
michael.krog <michael.krog@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2011-12-09 13:54:12 +0000

    Removed intialnumbers for order and invoice in favor of always being able to set them via SequenceProcessors.

--- shopsystem-core/src/main/java/dk/apaq/shopsystem/entity/Organisation.java
+++ shopsystem-core/src/main/java/dk/apaq/shopsystem/entity/Organisation.java
@@ -1,13 +1,6 @@
 package dk.apaq.shopsystem.entity;
 
-import java.io.Serializable;
-import java.util.Date;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Temporal;
-import org.hibernate.annotations.GenericGenerator;
 
 /**
  * Specifies an organisation
@@ -15,8 +8,6 @@ import org.hibernate.annotations.GenericGenerator;
 @Entity
 public class Organisation extends AbstractCompany {
 
-    private long initialOrdernumber = 1;
-    private long initialInvoiceNumber = 1;
     private int defaultPaymentPeriodInDays = 8;
     private String currency = "USD";
     private String annexNote;
@@ -29,22 +20,7 @@ public class Organisation extends AbstractCompany {
     public void setCurrency(String currency) {
         this.currency = currency;
     }
-    public long getInitialInvoiceNumber() {
-        return initialInvoiceNumber;
-    }
-
-    public void setInitialInvoiceNumber(long initialInvoiceNumber) {
-        this.initialInvoiceNumber = initialInvoiceNumber;
-    }
-
-    public long getInitialOrdernumber() {
-        return initialOrdernumber;
-    }
-
-    public void setInitialOrdernumber(long initialOrdernumber) {
-        this.initialOrdernumber = initialOrdernumber;
-    }
-
+    
     public int getDefaultPaymentPeriodInDays() {
         return defaultPaymentPeriodInDays;
     }
