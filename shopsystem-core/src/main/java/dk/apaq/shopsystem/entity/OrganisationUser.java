6cbe04718df36e8e9a86eb75bf37e2343a9cfb0a
michael.krog <michael.krog@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2011-12-15 15:23:50 +0000

    Added OrganisationUser

---
+++ shopsystem-core/src/main/java/dk/apaq/shopsystem/entity/OrganisationUser.java
@@ -0,0 +1,101 @@
+package dk.apaq.shopsystem.entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ *
+ * @author michael
+ */
+@Entity
+public class OrganisationUser implements ContentEntity {
+ 
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    protected String id;
+
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    @Column(nullable=false)
+    private Date dateCreated = new Date();
+
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    @Column(nullable=false)
+    private Date dateChanged = new Date();
+    
+    @ManyToOne(optional=false)
+    private Organisation organisation;
+    
+    @ManyToOne(optional=false)
+    private SystemUser user; 
+    
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Fetch(FetchMode.SUBSELECT)
+    private List<String> roles = new ArrayList<String>();
+
+    public OrganisationUser() {
+    }
+
+    public OrganisationUser(SystemUser systemUser) {
+        this.user = systemUser;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDateChanged() {
+        return dateChanged;
+    }
+
+    public void setDateChanged(Date dateChanged) {
+        this.dateChanged = dateChanged;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public Organisation getOrganisation() {
+        return organisation;
+    }
+
+    public void setOrganisation(Organisation organisation) {
+        this.organisation = organisation;
+    }
+
+    public SystemUser getUser() {
+        return user;
+    }
+
+    public void setUser(SystemUser user) {
+        this.user = user;
+    }
+
+    
+}
