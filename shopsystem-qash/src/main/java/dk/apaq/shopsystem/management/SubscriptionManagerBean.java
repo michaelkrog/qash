c003569d4304910a9d66c5191dfe7b5452721dbc
michael.krog <michael.krog@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2012-01-16 14:07:52 +0000

    More work on subscriptio0n management

--- shopsystem-qash/src/main/java/dk/apaq/shopsystem/management/SubscriptionManagerBean.java
+++ shopsystem-qash/src/main/java/dk/apaq/shopsystem/management/SubscriptionManagerBean.java
@@ -11,6 +11,7 @@ import dk.apaq.shopsystem.entity.Payment;
 import dk.apaq.shopsystem.entity.PaymentType;
 import dk.apaq.shopsystem.entity.Subscription;
 import dk.apaq.shopsystem.entity.SubscriptionPricingType;
+import dk.apaq.shopsystem.entity.SystemUser;
 import dk.apaq.shopsystem.entity.Tax;
 import dk.apaq.shopsystem.pay.PaymentException;
 import dk.apaq.shopsystem.pay.PaymentGateway;
@@ -21,7 +22,14 @@ import java.text.NumberFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import javax.persistence.EntityManager;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 
 /**
  *
@@ -29,15 +37,25 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class SubscriptionManagerBean {
 
-    @Autowired
-    private SystemService service;
-    @Autowired
-    private PaymentGateway paymentGateway;
-    private NumberFormat orderNumberFormatter = NumberFormat.getIntegerInstance();
-    private NumberFormat feeFormatter = NumberFormat.getPercentInstance();
+    private static final Logger LOG = LoggerFactory.getLogger(SubscriptionManagerBean.class);
+    
+    private final EntityManager em;
+    private final SystemService service;
+    private final PaymentGateway paymentGateway;
+    private final NumberFormat orderNumberFormatter = NumberFormat.getIntegerInstance();
+    private final NumberFormat feeFormatter = NumberFormat.getPercentInstance();
+    private final NumberFormat amountFormatter = NumberFormat.getCurrencyInstance(Locale.ENGLISH);
+    private final MailSender mailSender;
+    private final SimpleMailMessage templateMessage;
 
-    public SubscriptionManagerBean() {
-        orderNumberFormatter.setMinimumIntegerDigits(4);
+    @Autowired
+    public SubscriptionManagerBean(EntityManager em, SystemService service, PaymentGateway paymentGateway, MailSender mailSender, SimpleMailMessage templateMessage) {
+        this.em = em;
+        this.service = service;
+        this.paymentGateway = paymentGateway;
+        this.mailSender = mailSender;
+        this.templateMessage = templateMessage;
+        this.orderNumberFormatter.setMinimumIntegerDigits(4);
     }
 
     public void maintainSubscriptions() {
@@ -45,75 +63,143 @@ public class SubscriptionManagerBean {
         //through the entitymanager instead of traversing all organsiations through 
         //the service.
 
-        List<Subscription> subscriptions = null; //Get subscriptions ready to be charged order by organisation
+        String sql = "select s from Subscription s where "
+                + "enabled == true and autoRenew == true";
+
+        List<Subscription> subscriptions = em.createQuery(sql).getResultList();
         for (Subscription subscription : subscriptions) {
 
-            OrganisationService orgService = service.getOrganisationService(subscription.getOrganisation());
-            
-            CustomerRelationship customerRelationship = subscription.getCustomer();
-            String customerCurrency = customerRelationship.getCustomer().getCurrency();
-            Country customerCountry = Country.getCountry(customerRelationship.getCustomer().getCountryCode(), Locale.getDefault());
-
-            Order order = new Order();
-
-            //1: Find out how much to collect
-            if (subscription.getPricingType() == SubscriptionPricingType.QashUsageBase) {
-                //1a: if qashUsageBased subscription calculate usage fee
-                order.setCurrency(customerCurrency);
-                double revenue = getUsageFee(subscription);
-                double fee = revenue * customerRelationship.getCustomer().getFeePercentage();
-
-                order.addOrderLine("Fee for revenue (" + feeFormatter.format(fee) + ")", 1, fee, getTaxBasedOnCountry(customerCountry));
-            } else {
-                order.setCurrency(subscription.getCurrency());
-            
-                //1b: else use price specified on subscription
-                order.addOrderLine("Subscription", 1, subscription.getPrice(), getTaxBasedOnCountry(customerCountry));
+            //ensure that the subscription is to be collected for
+            if(!isDueForCollection(subscription)) {
+                continue;
             }
             
+            OrganisationService orgService = service.getOrganisationService(subscription.getOrganisation());
+            SystemUser adminUser = getAdminUserForOrganisation(subscription.getCustomer().getCustomer());
+
+            //1: generate order
+            Order order = generateOrderFromSubscription(subscription);
 
             //2: Create order
             String id = orgService.getOrders().create(order);
             order = orgService.getOrders().read(id);
             
-            //3: If missing payment info send user mail
+            //3:Update charge date on subscription
+            subscription.setDateCharged(new Date());
+            orgService.getSubscriptions().update(subscription);
+
+            //4: If missing payment info send user mail
             if (subscription.getSubscriptionPaymentId() == null) {
-                //TODO Unable to authorize payment - send mail to user regarding missing payment information
+                if (adminUser != null) {
+                    //TODO Unable to authorize payment - send mail to user regarding missing payment information
+                    SimpleMailMessage msg = this.templateMessage == null ? new SimpleMailMessage() : new SimpleMailMessage(this.templateMessage);
+                    msg.setSubject("New account");
+                    msg.setTo(adminUser.getEmail());
+                    msg.setText(
+                            "Dear " + adminUser.getDisplayName()
+                            + "\n\nWe are about to withdraw a payment for your subscription but we dont have your payment information.\n"
+                            + "Please go to the following link in order to fulfill your payment.\n"
+                            + "http://qashapp.com/payment.html?id="+order.getId()+"\n\n"
+                            + "If we are not able to withdraw the payment within 14 days we will automatically cancel your subscription.\n\n"
+                            + "Best Regards\n"
+                            + "The Qash team.");
+                    try {
+                        this.mailSender.send(msg);
+                    } catch (MailException ex) {
+                        // simply log it and go on...
+                        LOG.error("Unable to send mail.", ex);
+                    }
+                }
                 continue;
             }
 
             double paymentAmount = order.getTotalWithTax();
-            
-            //4: If unable to authorize recurring payment send user mail
+
+            //5: If unable to authorize recurring payment send user mail
             try {
                 paymentGateway.recurring(orderNumberFormatter.format(order.getNumber()), (int) (paymentAmount * 100), order.getCurrency(), false, subscription.getSubscriptionPaymentId());
             } catch (PaymentException ex) {
-                //TODO Unable to authorize payment - send user a mail regarding payment trouble
+                if (adminUser != null) {
+                    //Unable to authorize payment - send mail to user regarding missing payment information
+                    SimpleMailMessage msg = this.templateMessage == null ? new SimpleMailMessage() : new SimpleMailMessage(this.templateMessage);
+                    msg.setSubject("New account");
+                    msg.setTo(adminUser.getEmail());
+                    msg.setText(
+                            "Dear " + adminUser.getDisplayName()
+                            + "\n\nWe were unable withdraw a payment for your subscription using the payment information you gave us earlier.\n"
+                            + "Please go to the following link in order to fulfill your payment.\n"
+                            + "http://qashapp.com/payment.html?id="+order.getId()+"\n\n"
+                            + "If we are not able to withdraw the payment within 14 days we will automatically cancel your subscription.\n\n"
+                            + "Best Regards\n"
+                            + "The Qash team.");
+                    try {
+                        this.mailSender.send(msg);
+                    } catch (MailException ex2) {
+                        // simply log it and go on...
+                        LOG.error("Unable to send mail.", ex);
+                    }
+                }
                 continue;
             }
 
-            //5: register payments for order
+            //6: register payments for order
             Payment payment = new Payment();
             payment.setAmount(paymentAmount);
             payment.setOrderId(order.getId());
             payment.setPaymentType(PaymentType.Card);
             orgService.getPayments().create(payment);
-            
-            //6:Update charge date on subscription
-            subscription.setDateCharged(new Date());
-            orgService.getSubscriptions().update(subscription);
 
             //7: capture amount
             paymentGateway.capture((int) (paymentAmount * 100), subscription.getSubscriptionPaymentId());
 
-            //8: Send invoice/receipt
-            //TODO Send invoice
+            //8: Send receipt
+            if (adminUser != null) {
+                //Unable to authorize payment - send mail to user regarding missing payment information
+                SimpleMailMessage msg = this.templateMessage == null ? new SimpleMailMessage() : new SimpleMailMessage(this.templateMessage);
+                msg.setSubject("New account");
+                msg.setTo(adminUser.getEmail());
+                msg.setText(
+                        "Dear " + adminUser.getDisplayName()
+                        + "\n\nWe have withdrawn a payment for your subscription with us.\n\n"
+                        + "Amount: " + amountFormatter.format(paymentAmount) + "\n\n"
+                        + "Please login to your account to retrieve invoices when needed.\n"
+                        + "http://qashapp.com\n\n"
+                        + "Best Regards\n"
+                        + "The Qash team.");
+                try {
+                    this.mailSender.send(msg);
+                } catch (MailException ex) {
+                    // simply log it and go on...
+                    LOG.error("Unable to send mail.", ex);
+                }
+            }
+
+            //9: Sleep a bit to make time for other threads also
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ex) {
+            }
 
         }
 
     }
 
-    private double getUsageFee(Subscription subscription) {
+    public SystemUser getAdminUserForOrganisation(Organisation organisation) {
+        return null;
+    }
+
+    public Tax getTaxBasedOnCountry(Country country) {
+        //TODO This should actually be chosen from taxes the organisation has registered.
+        //Right now this is harcoded for salers inside EU.
+        if (country.isWithinEu()) {
+            return new Tax("Vat", 25);
+        } else {
+            return null;
+        }
+    }
+
+    /********* METHODS THAT SHOULD BE MOVED TO A SUBSCRIPTION UTILITY CLASS ************/
+    public double getUsageFee(Subscription subscription) {
 
         Date dateFrom = subscription.getDateCharged() == null ? subscription.getDateCreated() : subscription.getDateCharged();
         Filter orderFilter = new AndFilter(new CompareFilter("status", OrderStatus.Completed, CompareFilter.CompareType.Equals),
@@ -136,14 +222,57 @@ public class SubscriptionManagerBean {
 
         return fee;
     }
-    
-    private Tax getTaxBasedOnCountry(Country country) {
-        //TODO This should actually be chosen from taxes the organisation has registered.
-        //Right now this is harcoded for salers inside EU.
-        if(country.isWithinEu()) {
-            return new Tax("Vat", 25);
+
+    public Order generateOrderFromSubscription(Subscription subscription) {
+        CustomerRelationship customerRelationship = subscription.getCustomer();
+        Country customerCountry = Country.getCountry(customerRelationship.getCustomer().getCountryCode(), Locale.getDefault());
+
+        String customerCurrency = customerRelationship.getCustomer().getCurrency();
+
+        Order order = new Order();
+
+        //1: Find out how much to collect
+        if (subscription.getPricingType() == SubscriptionPricingType.QashUsageBase) {
+            //1a: if qashUsageBased subscription calculate usage fee
+            order.setCurrency(customerCurrency);
+            double revenue = getUsageFee(subscription);
+            double fee = revenue * customerRelationship.getCustomer().getFeePercentage();
+
+            order.addOrderLine("Fee for revenue (" + feeFormatter.format(fee) + ")", 1, fee, getTaxBasedOnCountry(customerCountry));
         } else {
-            return null;
+            order.setCurrency(subscription.getCurrency());
+
+            //1b: else use price specified on subscription
+            order.addOrderLine("Subscription", 1, subscription.getPrice(), getTaxBasedOnCountry(customerCountry));
         }
+        return order;
+    }
+
+    public boolean isDueForCollection(Subscription subscription) {
+        Date startOfCurrentInterval = subscription.getDateCharged() == null ? subscription.getDateCreated() : subscription.getDateCharged();
+
+        DateTime cutOffTime = new DateTime(startOfCurrentInterval.getTime());
+
+        switch (subscription.getIntervalUnit()) {
+            case Hour:
+                cutOffTime.plusHours(subscription.getInterval());
+                break;
+            case Day:
+                cutOffTime.plusDays(subscription.getInterval());
+                break;
+            case Week:
+                cutOffTime.plusWeeks(subscription.getInterval());
+                break;
+            case Month:
+                cutOffTime.plusMonths(subscription.getInterval());
+                break;
+            case Year:
+                cutOffTime.plusYears(subscription.getInterval());
+                break;
+        }
+
+        //If time has passed cutOffTime then return true
+        return cutOffTime.isBeforeNow();
+
     }
 }
