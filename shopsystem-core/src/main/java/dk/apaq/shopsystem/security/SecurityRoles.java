c4eeec311e18601a4d9f523820319d167fbb4fe4
michael.krog <michael.krog@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2012-04-03 12:52:45 +0000

    More javadoc.

--- shopsystem-core/src/main/java/dk/apaq/shopsystem/security/SecurityRoles.java
+++ shopsystem-core/src/main/java/dk/apaq/shopsystem/security/SecurityRoles.java
@@ -5,5 +5,18 @@ package dk.apaq.shopsystem.security;
  * @author michaelzachariassenkrog
  */
 public enum SecurityRoles {
-    ROLE_ADMIN, ROLE_ORGADMIN, ROLE_ORGPAYER
+    /**
+     * Role for a system administrator
+     */
+    ROLE_ADMIN, 
+    
+    /**
+     * Role for an organisation administrator
+     */
+    ROLE_ORGADMIN, 
+    
+    /**
+     * Role for the on that should recieve payment information for an organisation.
+     */
+    ROLE_ORGPAYER
 }
