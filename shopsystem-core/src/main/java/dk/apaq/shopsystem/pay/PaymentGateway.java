98c033044b9c6522662fa38f326b6d14c0092e9e
michael.krog <michael.krog@1751a33b-01d8-4ce1-aa75-e1639f6c8635>
2012-04-03 12:58:07 +0000

    More javadoc.

--- shopsystem-core/src/main/java/dk/apaq/shopsystem/pay/PaymentGateway.java
+++ shopsystem-core/src/main/java/dk/apaq/shopsystem/pay/PaymentGateway.java
@@ -6,14 +6,48 @@ package dk.apaq.shopsystem.pay;
  */
 public interface PaymentGateway {
     
+    /**
+     * Captures an already authorized amount.
+     * @param amountInCents The amount in cents(smallest denominator in the currency.)
+     * @param transactionId The already authorized transaction.
+     */
     public void capture(long amountInCents, String transactionId);
+    
+    /**
+     * Authorizes a new amount for a transaction already authorized for recurrings transactions.
+     * @param orderNumber The ordernumber.
+     * @param amountInCents The amount in cents(smallest denominator in the currency.)
+     * @param currency The currency code.
+     * @param autocapture Wether to autocapture the amount.
+     * @param transactionId The transactionid.
+     */
     public void recurring(String orderNumber, long amountInCents, String currency, boolean autocapture, String transactionId);
 
+    /**
+     * Renews an existing auhorization.
+     * @param amountInCents
+     * @param transactionId 
+     */
     public void renew(long amountInCents, String transactionId);
 
+    /**
+     * Refund a transaction
+     * @param amountInCents The amount in cents(smallest denominator in the currency.)
+     * @param transactionId 
+     */
     public void refund(long amountInCents, String transactionId);
+    
+    /**
+     * Cancels an transaction.
+     * @param transactionId 
+     */
     public void cancel(String transactionId);
 
+    /**
+     * Retrieves all information about the transaction.
+     * @param transactionId
+     * @return 
+     */
     public PaymentInformation getPaymentInformation(String transactionId);
 
     
